5f4ff5671588c0dffdb87d1ea1ec5fc8cafd8fea
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-12 20:59:41 +0600

    mail part complete

--- src/Page/Register.jsx
+++ src/Page/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { RiUserLine } from "react-icons/ri";
+import { IoMailOpenOutline } from "react-icons/io5";
 
 const Register = () => {
   return (
@@ -8,17 +9,32 @@ const Register = () => {
 <form className="form lg:w-[440px] w-full bg-[#FFF] dark:bg-[#1F2128] p-10 rounded-[16px]">
 <h2 className='lg:text-24 text-lg font-bold font-popins pb-1 text-primery text-center dark:text-[#F1F1F1] '>Create an account</h2>
 <p className='text-sm font-normal font-popins text-[#7E7E8F] text-center dark:text-[#8B8B93] pb-7.5 '>You are welcome!</p>
+
+
+
 {/* ------------name part---------- */}
 <div className="name">
   <h2 className='text-sm font-normal font-popins text-primery dark:text-[#F1F1F1] pb-2'>Your Name</h2>
   <div className="both flex justify-between">
-    <div className="name1 w-[170px] px-2 h-12 border border-[#E8EDF2] rounded-[8px] flex justify-center items-center">
+    <div className="name1 w-[170px] px-2 h-12 border border-[#E8EDF2] dark:border-[#313442] rounded-[8px] flex justify-center items-center">
       <input type="text" placeholder='First Name' className='text-sm w-full font-popins text-[#C6CBD9] dark:text-[#2C2C35]'/>
      <RiUserLine className='text-base text[#9A9AAF] dark:text-[#64646F]'/>
     </div>
-    <div className="name1 w-[170px] px-2 h-12 border border-[#E8EDF2] rounded-[8px] flex justify-center items-center">
+    <div className="name1 w-[170px] px-2 h-12 border border-[#E8EDF2] dark:border-[#313442] rounded-[8px] flex justify-center items-center">
       <input type="text" placeholder='Last name' className='text-sm w-full font-popins text-[#C6CBD9] dark:text-[#2C2C35]'/>
      <RiUserLine className='text-base text[#9A9AAF] dark:text-[#64646F]'/>
+    </div>
+  </div>
+</div>
+
+
+{/* ------------mail part---------- */}
+<div className="mail">
+  <h2 className='text-sm font-normal font-popins text-primery dark:text-[#F1F1F1] pb-2 mt-5'>E-mail</h2>
+  <div className="both flex justify-between">
+    <div className="name1 w-full px-2 h-12 border border-[#E8EDF2] dark:border-[#313442] rounded-[8px] flex justify-center items-center">
+      <input type="text" placeholder='Email' className='text-sm w-full font-popins text-[#C6CBD9] dark:text-[#2C2C35]'/>
+     <IoMailOpenOutline  className='text-base text[#9A9AAF] dark:text-[#64646F]'/>
     </div>
   </div>
 </div>
