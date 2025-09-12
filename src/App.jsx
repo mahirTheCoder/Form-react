7d40f84d7a3d99bc5d1ede87c8730b2113a48184
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-12 18:33:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router'
+
+const App = () => {
+const myRoute = createBrowserRouter(createRoutesFromElements(
+  <Route>
+    
+  </Route>
+))
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
