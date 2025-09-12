fbfeda0dd3f7aac8d4d5a9669f3af2f0bd0c0327
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-12 20:51:37 +0600

    2

--- src/Page/Register.jsx
+++ src/Page/Register.jsx
@@ -4,20 +4,21 @@ import { RiUserLine } from "react-icons/ri";
 const Register = () => {
   return (
     <>
-<section id='form' className=' h-screen flex justify-center items-center bg-[#E8EDF2]  '>
-<form className="form lg:w-[440px] w-full bg-[#FFF] p-10 rounded-[16px]">
-<h2 className='lg:text-24 text-lg font-bold font-popins pb-1 text-primery text-center '>Create an account</h2>
-<p className='text-sm font-normal font-popins text-[#7E7E8F] text-center pb-7.5 '>You are welcome!</p>
+<section id='form' className=' h-screen flex justify-center items-center bg-[#E8EDF2] dark:bg-[#313442] '>
+<form className="form lg:w-[440px] w-full bg-[#FFF] dark:bg-[#1F2128] p-10 rounded-[16px]">
+<h2 className='lg:text-24 text-lg font-bold font-popins pb-1 text-primery text-center dark:text-[#F1F1F1] '>Create an account</h2>
+<p className='text-sm font-normal font-popins text-[#7E7E8F] text-center dark:text-[#8B8B93] pb-7.5 '>You are welcome!</p>
+{/* ------------name part---------- */}
 <div className="name">
-  <h2 className='text-sm font-normal font-popins text-primery pb-2'>Your Name</h2>
+  <h2 className='text-sm font-normal font-popins text-primery dark:text-[#F1F1F1] pb-2'>Your Name</h2>
   <div className="both flex justify-between">
-    <div className="name1 px-2 h-12 border border-[#E8EDF2] rounded-[8px] flex justify-center items-center">
-      <input type="text" placeholder='First Name' className='text-sm w-full font-popins text-[#C6CBD9]'/>
-     <RiUserLine className='text-base text[#9A9AAF]'/>
+    <div className="name1 w-[170px] px-2 h-12 border border-[#E8EDF2] rounded-[8px] flex justify-center items-center">
+      <input type="text" placeholder='First Name' className='text-sm w-full font-popins text-[#C6CBD9] dark:text-[#2C2C35]'/>
+     <RiUserLine className='text-base text[#9A9AAF] dark:text-[#64646F]'/>
     </div>
-    <div className="name1 px-2 h-12 border border-[#E8EDF2] rounded-[8px] flex justify-center items-center">
-      <input type="text" placeholder='First Name' className='text-sm w-full font-popins text-[#C6CBD9]'/>
-     <RiUserLine className='text-base text[#9A9AAF]'/>
+    <div className="name1 w-[170px] px-2 h-12 border border-[#E8EDF2] rounded-[8px] flex justify-center items-center">
+      <input type="text" placeholder='Last name' className='text-sm w-full font-popins text-[#C6CBD9] dark:text-[#2C2C35]'/>
+     <RiUserLine className='text-base text[#9A9AAF] dark:text-[#64646F]'/>
     </div>
   </div>
 </div>
