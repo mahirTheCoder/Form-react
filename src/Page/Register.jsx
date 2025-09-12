ec532808509618c2f6788d040240d3b700ed0a77
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-12 21:06:22 +0600

    phone number part complete

--- src/Page/Register.jsx
+++ src/Page/Register.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { RiUserLine } from "react-icons/ri";
 import { IoMailOpenOutline } from "react-icons/io5";
+import { BiPhoneCall } from "react-icons/bi";
 
 const Register = () => {
   return (
@@ -17,11 +18,11 @@ const Register = () => {
   <h2 className='text-sm font-normal font-popins text-primery dark:text-[#F1F1F1] pb-2'>Your Name</h2>
   <div className="both flex justify-between">
     <div className="name1 w-[170px] px-2 h-12 border border-[#E8EDF2] dark:border-[#313442] rounded-[8px] flex justify-center items-center">
-      <input type="text" placeholder='First Name' className='text-sm w-full font-popins text-[#C6CBD9] dark:text-[#2C2C35]'/>
+      <input type="text" placeholder='First Name' className='text-sm w-full font-popins text-[#C6CBD9] dark:text-[#2C2C35]  border-none outline-none'/>
      <RiUserLine className='text-base text[#9A9AAF] dark:text-[#64646F]'/>
     </div>
     <div className="name1 w-[170px] px-2 h-12 border border-[#E8EDF2] dark:border-[#313442] rounded-[8px] flex justify-center items-center">
-      <input type="text" placeholder='Last name' className='text-sm w-full font-popins text-[#C6CBD9] dark:text-[#2C2C35]'/>
+      <input type="text" placeholder='Last name' className='text-sm w-full font-popins text-[#C6CBD9] dark:text-[#2C2C35]  border-none outline-none'/>
      <RiUserLine className='text-base text[#9A9AAF] dark:text-[#64646F]'/>
     </div>
   </div>
@@ -33,8 +34,20 @@ const Register = () => {
   <h2 className='text-sm font-normal font-popins text-primery dark:text-[#F1F1F1] pb-2 mt-5'>E-mail</h2>
   <div className="both flex justify-between">
     <div className="name1 w-full px-2 h-12 border border-[#E8EDF2] dark:border-[#313442] rounded-[8px] flex justify-center items-center">
-      <input type="text" placeholder='Email' className='text-sm w-full font-popins text-[#C6CBD9] dark:text-[#2C2C35]'/>
+      <input type="email" placeholder='Email' className='text-sm w-full font-popins text-[#C6CBD9] dark:text-[#2C2C35]  border-none outline-none'/>
      <IoMailOpenOutline  className='text-base text[#9A9AAF] dark:text-[#64646F]'/>
+    </div>
+  </div>
+</div>
+
+
+{/* ------------Phone numbers part---------- */}
+<div className="mail">
+  <h2 className='text-sm font-normal font-popins text-primery dark:text-[#F1F1F1] pb-2 mt-5'>Phone numbers</h2>
+  <div className="both flex justify-between">
+    <div className="name1 w-full px-2 h-12 border border-[#E8EDF2] dark:border-[#313442] rounded-[8px] flex justify-center items-center">
+      <input type="number" placeholder='(+01)' className='text-sm w-full font-popins text-[#C6CBD9] dark:text-[#2C2C35] border-none outline-none '/>
+     <BiPhoneCall  className='text-base text[#9A9AAF] dark:text-[#64646F]'/>
     </div>
   </div>
 </div>
